cefdcdf185764ab09051062b4ab408b6ad14b898
cinnamondev <justpromptiguess@gmail.com>
2024-12-12 03:07:05 +0000

    a foray into rust multithreading

--- src/day/day11.rs
+++ src/day/day11.rs
@@ -1,26 +1,79 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Read};
+use std::thread;
+use std::thread::{JoinHandle, ScopedJoinHandle};
+use std::time::Instant;
 
 pub fn run() {
     let mut file = File::open("./input/day11").expect("no read :(");
     let mut string = String::new();
     file.read_to_string(&mut string).expect("weeeeh");
 
-    let part1 = parse_line(&string, 25);
+    let before = Instant::now();
+    let (part1,part2) = parse_line(&string);
+    println!("Single threaded time: {:.2?}", before.elapsed());
     println!(",1 {}", part1);
+    println!(",2 {}", part2);
 
-    let part2 = parse_line(&string, 75);
+    let before = Instant::now();
+    let (part1,part2) = parse_line_multithread(&string);
+    println!("Multi threaded time: {:.2?}", before.elapsed());
+    println!(",1 {}", part1);
     println!(",2 {}", part2);
 }
+fn parse_line_multithread(line: &String) -> (u64,u64) {
+    let mut p1_sum = 0;
+    let mut p2_sum = 0;
+    thread::scope(|s| {
+        let strings: Vec<&str> = line.split_whitespace().collect();
+        let mut handles: Vec<ScopedJoinHandle<(u64,u64)>> = Vec::new();
+        for string in strings {
+            handles.push(
+                s.spawn(move || {
+                    let mut stone_map: HashMap<u64,u64> = HashMap::new();
+                    stone_map.entry(string.parse::<u64>().unwrap()).or_insert(1);
+                    let mut p1_sum = 0;
+                    for i in 0..75 {
+                        if i == 25 { p1_sum += stone_map.values().sum::<u64>(); }
 
-fn parse_line(line: &String, blinks: usize) -> usize {
+                        let mut to_parse_next: HashMap<u64,u64> = HashMap::new();
+                        //println!("stone {n} , pass {}",i+1);
+                        for (rock,count) in stone_map {
+                            let (original, opt_rock) = next_state(rock);
+                            to_parse_next.entry(original)
+                                .and_modify(|c| *c+=count )
+                                .or_insert(count);
+                            if let Some(new_rock) = opt_rock {
+                                to_parse_next.entry(new_rock)
+                                    .and_modify(|c| *c+=count )
+                                    .or_insert(count);
+                            }
+                        }
+                        stone_map = to_parse_next;
+                    }
+                    (p1_sum, stone_map.values().sum::<u64>())
+                })
+            );
+        }
+        for handle in handles {
+            let (sum_rock_1,sum_rock_2) = handle.join().expect("uh ohhh");
+            p1_sum += sum_rock_1; p2_sum += sum_rock_2;
+        }
+    });
+    (p1_sum, p2_sum)
+}
+
+fn parse_line(line: &String) -> (u64,u64) {
     line.split_whitespace().enumerate().map(|(n,string_stone)| {
-        let mut stone_map: HashMap<u64,usize> = HashMap::new();
+        let mut stone_map: HashMap<u64,u64> = HashMap::new();
         stone_map.entry(string_stone.parse::<u64>().unwrap()).or_insert(1);
 
-        for i in 0..blinks {
-            let mut to_parse_next: HashMap<u64,usize> = HashMap::new();
+        let mut part1_sum = 0;
+        for i in 0..75 {
+            if i == 25 { part1_sum = stone_map.values().sum(); }
+
+            let mut to_parse_next: HashMap<u64,u64> = HashMap::new();
             //println!("stone {n} , pass {}",i+1);
             for (rock,count) in stone_map {
                 let (original, opt_rock) = next_state(rock);
@@ -35,8 +88,12 @@ fn parse_line(line: &String, blinks: usize) -> usize {
             }
             stone_map = to_parse_next;
         }
-        stone_map.values().sum::<usize>()
-    }).sum()
+        (part1_sum, stone_map.values().sum::<u64>())
+    }).fold((0,0), |(mut part1, mut part2), (new_1,new_2)| {
+        part1 += new_1;
+        part2 += new_2;
+        (part1,part2)
+    })
 }
 
 fn next_state(rock_value: u64) -> (u64, Option<u64>) {
